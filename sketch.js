d04fc740c5d83e661eb029fe92ebe99456eb9efd
Thomas S <thomas.swanson107@gmail.com>
2023-03-31 23:06:08 -0700

    Fix submit button

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 const container = document.getElementById('container');
+const input = document.getElementById('input');
 
 function drawGrid(size) {
-    let boxSize = Math.floor(((640 * 640) / size) / 640);
+    let boxSize = Math.round(((640 * 640) / size) / 640);
     for (let i = 0; i < size; i++) {
         let div = document.createElement('div');
         for (let n = 0; n < size; n++) {
@@ -10,13 +11,36 @@ function drawGrid(size) {
             box.style.height = `${boxSize - 2}.px`;
             box.style.width = `${boxSize - 2}.px`;
             div.append(box);
+            container.width = (boxSize * size);
+            container.height = (boxSize * size);
 
         }
 
     container.append(div);
     }
 
-    return boxSize;
+    
 }
 
-let boxes = document.onload = drawGrid();
+function destroyGrid() {
+    const element = document.getElementById('div');
+    element.removeChild;
+}
+
+//let boxes = document.onload = drawGrid();
+
+function eventHandler(event) {
+    //destroyGrid();
+    if (event === 'submit') {
+        let num = input.value;
+        if (Number.isInteger(Math.floor(parseInt(num)))) {
+            if (num < 2 || num > 100) {
+                alert("Min: 2. Max: 100")
+            } else {
+                drawGrid(num);
+            }
+        } else {
+            alert("No valid value in input");
+        }
+    }
+}
