35ca7584fd33f0d19e334de59ee43a2f71da0b60
Thomas S <thomas.swanson107@gmail.com>
2023-04-01 13:36:24 -0700

    Add drawing functionality and edit page text

--- sketch.js
+++ sketch.js
@@ -9,20 +9,33 @@ function drawGrid(size) {
     container.style.width = (boxSize * size + 'px');
     container.style.height = (boxSize * size + 'px');
     for (let i = 0; i < size; i++) {
+    
         let div = document.createElement('div');
         for (let j = 0; j < size; j++) {
             let box = document.createElement('div');
             box.setAttribute("class", "box");
-            box.style.height = `${boxSize - 2}.px`;
-            box.style.width = `${boxSize - 2}.px`;
+            box.style.height = `${boxSize}.px`;
+            box.style.width = `${boxSize}.px`;
+            box.style.backgroundColor = 'white';
+            box.style.userSelect = 'none';
+            box.addEventListener('mouseover', function(e) { changeColor(e, this); });
             div.append(box);   
         }
     container.append(div);
     }
-    
-
-    
 }
+
+
+function changeColor(event, element) {
+    if (event.buttons === 1) {
+        if (element.style.backgroundColor === 'white') {
+            element.style.backgroundColor = 'black';
+        } else if (element.style.backgroundColor === 'black') {
+            element.style.backgroundColor = 'white';
+        }
+    }
+}
+
 
 function destroyGrid() {
     console.log("Grid Destroy Starting...");
@@ -32,17 +45,14 @@ function destroyGrid() {
         destroyBox = document.querySelector('.box');
         destroyBox.remove()
     }
-
-    
-    
 }
 
 function eventHandler(event) {
     if (event === 'submit') {
         let num = input.value;
         if (Number.isInteger(Math.floor(parseInt(num)))) {
-            if (num < 2 || num > 144) {
-                alert("Min: 2. Max: 144")
+            if (num < 2 || num > 100) {
+                alert("Number must be at least 2 and no greater than 100")
             } else {
                 drawGrid(num);
             }
