c828e6b26fee558dc94ed8e6270363e85d57cde1
Thomas S <thomas.swanson107@gmail.com>
2023-03-31 23:59:24 -0700

    Fix destroy grid works

--- sketch.js
+++ sketch.js
@@ -4,47 +4,50 @@ const input = document.getElementById('input');
 let gridShow = false;
 
 function drawGrid(size) {
+    destroyGrid();
     let boxSize = Math.round(((780 * 780) / size) / 780);
     container.style.width = (boxSize * size + 'px');
     container.style.height = (boxSize * size + 'px');
     for (let i = 0; i < size; i++) {
         let div = document.createElement('div');
-        for (let n = 0; n < size; n++) {
+        for (let j = 0; j < size; j++) {
             let box = document.createElement('div');
             box.setAttribute("class", "box");
             box.style.height = `${boxSize - 2}.px`;
             box.style.width = `${boxSize - 2}.px`;
-            div.append(box);
-            
+            div.append(box);   
         }
-
     container.append(div);
     }
-
-    return boxSize;
     
 
     
 }
 
 function destroyGrid() {
-    alert("Grid Destroyed");
-    let element = document.querySelectorAll('box');
-    element.remove();
+    console.log("Grid Destroy Starting...");
+    let boxes = document.querySelectorAll('.box');
+    boxes = Array.from(boxes);
+    for (i = 0; i < boxes.length; i++){
+        destroyBox = document.querySelector('.box');
+        destroyBox.remove()
+    }
+
+    
+    
 }
 
 function eventHandler(event) {
-    //destroyGrid();
     if (event === 'submit') {
         let num = input.value;
         if (Number.isInteger(Math.floor(parseInt(num)))) {
-            if (num < 2 || num > 100) {
-                alert("Min: 2. Max: 100")
+            if (num < 2 || num > 144) {
+                alert("Min: 2. Max: 144")
             } else {
                 drawGrid(num);
             }
         } else {
-            alert("No valid value in input");
+            alert("No valid value in input. Enter a number");
         }
     }
 }
